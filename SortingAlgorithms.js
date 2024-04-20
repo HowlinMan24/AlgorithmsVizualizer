fe21d0eaf522e17bbf8e8831fd9ea979122500a1
Hristijan.Mijalkov.LoL <115577273+HowlinMan@users.noreply.github.com>
2024-04-20 15:43:47 +0200

    Making some  changes to the CSS

--- SortingAlgorithms.js
+++ SortingAlgorithms.js
@@ -1,19 +1,19 @@
-var tableArray = []
-var tableReference
-
-const Node = () => {
-    return {
-        height: Math.ceil(Math.random() * 250),
-        width: 0.5,
-        backgroundColor: 'red',
-    }
-};
-
-function applyCustomStyle(cell, node) {
-    cell.style.width = node.width + 'px';
-    cell.style.height = node.height + 'px';
-    cell.style.backgroundColor = node.backgroundColor;
-}
+// var tableArray = []
+// var tableReference
+//
+// const Node = () => {
+//     return {
+//         height: Math.ceil(Math.random() * 250),
+//         width: 0.5,
+//         backgroundColor: 'red',
+//     }
+// };
+//
+// function applyCustomStyle(cell, node) {
+//     cell.style.width = node.width + 'px';
+//     cell.style.height = node.height + 'px';
+//     cell.style.backgroundColor = node.backgroundColor;
+// }
 
 var divArraySorting
 
@@ -23,16 +23,16 @@ window.onload = function () {
 };
 
 function fillUpDiv(divReference) {
-    for (var i = 0; i < 920; i++) {
-        var divElement = document.createElement('div')
+    for (let i = 0; i < 900; i++) {
+        let divElement = document.createElement('div');
         setDivAttributes(divElement)
         divReference.appendChild(divElement)
     }
 }
 
 function setDivAttributes(divReference) {
-    var height = (Math.random() * 90) + (Math.random() * 2)
-    var width = 1.2
+    let height = (Math.random() * 90) + (Math.random() * 2);
+    let width = 1.2;
     divReference.style.backgroundColor = 'red'
     divReference.style.width = width + 'px'
     divReference.style.height = height + 'vh'
@@ -40,3 +40,43 @@ function setDivAttributes(divReference) {
     divReference.style.alignSelf = 'flex-end'
 }
 
+function clearTable() {
+    let childrenTable = divArraySorting.children;
+    console.log(childrenTable)
+    for (let i = childrenTable.length - 1; i >= 0; i--) {
+        divArraySorting.removeChild(childrenTable[i])
+    }
+    console.log("Fill up after clear")
+    fillUpDiv(divArraySorting)
+}
+
+function startSort() {
+    // Bubble sort for start then expand
+    let children = Array.from(divArraySorting.children);
+    let length = children.length;
+    let i = 0;
+    const interval = setInterval(() => {
+        let swapped = false;
+        for (let j = 0; j < length - i - 1; j++) {
+            let height1 = parseFloat(getComputedStyle(children[j]).height);
+            let height2 = parseFloat(getComputedStyle(children[j + 1]).height);
+            if (height1 > height2) {
+                swap(children[j], children[j + 1]);
+                swapped = true;
+            }
+            console.log("sorting")
+        }
+        if (!swapped) {
+            clearInterval(interval);
+        }
+        i++;
+    }, 100);
+}
+
+
+function swap(element1, element2) {
+    const parent = element1.parentNode;
+    const nextSibling = element2.nextSibling;
+    parent.insertBefore(element2, element1);
+    parent.insertBefore(element1, nextSibling);
+}
